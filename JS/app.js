ef9795ccfe37a9f26f2eb99048c86fc501ada724
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-13 09:37:50 +0600

    Main Funcionalities Done

---
+++ JS/app.js
@@ -0,0 +1,59 @@
+// Get All Variables
+const productInput = document.getElementById('product-input')
+const productCont = document.getElementById('product-container')
+const searchBtn = document.getElementById('search-btn')
+
+/* ============== Add Product Button ============== */
+const addProduct = () => {
+    const product = productInput.value 
+
+    // Show in the UI 
+    showProduct(product)
+
+    // Add to Local Storage 
+    addInStorage(product)
+
+    // Emptify the Box 
+    productInput.value = ''
+}
+
+/* Function to Show in the UI */
+const showProduct = product => {
+    const item = document.createElement('p')
+    item.classList.add('lead')
+    item.innerText = product
+    productCont.appendChild(item)
+}
+
+/* Function to Store in Local Storage */
+const addInStorage = product => {
+    // Get the cartCont
+    const cart = getCart()
+    if (cart[product]) {
+        cart[product] = cart[product] + 1
+    } else {
+        cart[product] = 1
+    }
+    const cartString = JSON.stringify(cart)
+    localStorage.setItem('Cart Container' ,cartString)
+}
+
+/* Get Cart Function */
+const getCart = () => {
+    let cart = localStorage.getItem('Cart Container')
+    if (cart) {
+        cart = JSON.parse(cart)
+    } else {
+        cart = {}
+    }
+    return cart
+}
+
+/* Show Stored Product */
+const storedProduct = () => {
+    const cart = getCart()
+    for (const item in cart) {
+        showProduct(item)
+    }
+} 
+storedProduct()
